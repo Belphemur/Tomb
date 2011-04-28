a6479fbfaa5950a92ce00c850e689154524e08cc
Antoine Aflalo <antoineaf@gmail.com>
2011-04-28 21:44:51 +0200

    check if the use can use more than one time the deathloc

--- src/main/java/be/Balor/Listeners/PlayerListenerTomb.java
+++ src/main/java/be/Balor/Listeners/PlayerListenerTomb.java
@@ -55,8 +55,11 @@ public class PlayerListenerTomb extends PlayerListener {
 					Tomb tomb = worker.getTomb(p.getName());
 					if (tomb.hasSign(block)) {
 						Location toTp;
-						if ((toTp = tomb.getDeathLoc()) != null)
+						if ((toTp = tomb.getDeathLoc()) != null) {
 							p.teleport(toTp);
+							if (worker.getConfig().getBoolean("reset-deathloc", true))
+								tomb.setDeathLoc(null);
+						}
 					}
 				}
 			}
